f2d09df60bc8a09765e244730bfdceb4a7a51ebd
Svitlana <conbka@gmail.com>
2025-01-11 18:41:13 +0200

    added routes for  products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get "/products", to: "products#index"
+  # get "/products", to: "products#index"
+  # /////////////////////
+
+  # route  gaide
+  resources :brands, only: [:index, :show] do
+    resources :products, only: [:index, :show]
+  end
+
+  resource :basket, only: [:show, :update, :destroy]
+
+  resolve("Basket") { route_for(:basket) }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
