ecea9b3e89dfce76d10ab04c5f7793fc43bede54
SvitlanaBahlai <conbka@gmail.com>
2025-01-15 17:24:04 +0200

    2.7 Nested Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,39 @@
 Rails.application.routes.draw do
+  #route  gaide
+  # resources :brands, only: [:index, :show] do
+  #   resources :products, only: [:index, :show]
+  # end
+
+  # resource :basket, only: [:show, :update, :destroy]
+
+  # resolve("Basket") { route_for(:basket) }
+
+  # resources :magazines do
+  #   resources :ads
+  # end
+
+  # resources :photos, only: [:index, :create, :show]
+
+  #   resources :publishers do
+  #     resources :magazines do
+  #       resources :photos
+  #     end
+  #   end
+
+    resources :articles do
+      resources :comments, only: [:index, :new, :create]
+    end
+    resources :comments, only: [:show, :edit, :update, :destroy]
+
+
+    resources :applicants do
+      resources :comments, shallow: true
+    end
+  # get "/users/:id", to: "user#show"
+
   # get "/products", to: "products#index"
   # /////////////////////
 
-  # route  gaide
-  resources :brands, only: [:index, :show] do
-    resources :products, only: [:index, :show]
-  end
-
-  resource :basket, only: [:show, :update, :destroy]
-
-  resolve("Basket") { route_for(:basket) }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
