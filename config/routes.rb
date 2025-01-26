8c99d1d26d2f1e4998f9d3c05855cb046e5e9a5b
cvetok <conbka@gmail.com>
2025-01-26 18:47:04 +0200

    added crud 10_04

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,37 @@ Rails.application.routes.draw do
   # get "/products", to: "products#index"
   # /////////////////////
 
+
+  root "products#index"
+  
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+
+  get "/products/:id", to: "products#show"
+
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put   "/products/:id", to: "products#update"
+
+  delete "/products/:id", to: "products#destroy"
+
+  
+
+
+
+
+  # /////////////////////
+
+  # route  gaide
+  # resources :brands, only: [:index, :show] do
+  #   resources :products, only: [:index, :show]
+  # end
+
+  # resource :basket, only: [:show, :update, :destroy]
+
+  # resolve("Basket") { route_for(:basket) }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
