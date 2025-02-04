8d8b1fe1880e436ebd2e7fdde3a58ff6667b6248
SvitlanaBahlai <conbka@gmail.com>
2025-02-04 20:22:53 +0200

    chapter 10.5

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 docker compose up --build
 
-<<<<<<< HEAD
 docker-compose run --service-ports web bash
-=======
- bin/rails db:migrate
- 
- rails s -b 0.0.0.0
->>>>>>> 6505fe9 (edit readme file)
 
 rails s -b 0.0.0.0
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,115 +1,25 @@
 Rails.application.routes.draw do
-  get 'photos/search'
-  #route  gaide
-  # resources :brands, only: [:index, :show] do
-  #   resources :products, only: [:index, :show]
-  # end
-
-  # resource :basket, only: [:show, :update, :destroy]
-
-  # resolve("Basket") { route_for(:basket) }
-
-  # resources :magazines do
-  #   resources :ads
-  # end
-
-  # resources :photos, only: [:index, :create, :show]
-
-  #   resources :publishers do
-  #     resources :magazines do
-  #       resources :photos
-  #     end
-  #   end
-
-    # resources :articles do
-    #   resources :comments, only: [:index, :new, :create]
-    # end
-    # resources :comments, only: [:show, :edit, :update, :destroy]
-
-
-    # resources :applicants do
-    #   resources :comments, shallow: true
-    # end
-  # get "/users/:id", to: "user#show"
-
-  # get "/products", to: "products#index"
-  # /////////////////////
-
-
   root "products#index"
   
   get "/products", to: "products#index"
 
   get "/products/new", to: "products#new"
+
   post "/products", to: "products#create"
 
   get "/products/:id", to: "products#show"
 
   get "/products/:id/edit", to: "products#edit"
+
   patch "/products/:id", to: "products#update"
+  
   put   "/products/:id", to: "products#update"
 
   delete "/products/:id", to: "products#destroy"
 
-  
+  get "/blog/:title", to: "blog#show"
 
-
-
-
-  # /////////////////////
-
-  # route  gaide
-  # resources :brands, only: [:index, :show] do
-  #   resources :products, only: [:index, :show]
-  # end
-
-  # resource :basket, only: [:show, :update, :destroy]
-
-  # resolve("Basket") { route_for(:basket) }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-# resources :messages do
-#   resources :comments
-# end
-
-# resources :articles do
-#   resources :comments
-#   resources :images, only: :index
-# end
-
-# resources :magazines do
-#   resources :ads
-# end
-
-# resources :photos do
-#   member do
-#     get "preview"
-#   end
-# end
-
-# resources :photos do
-#   get "preview", on: :member
-# end
-
-# resources :photos do
-#   get "search", on: :collection
-# end
-
-# resources :photos do
-#   collection do
-#     get "search"
-#   end
-# end
-
-defaults format: :json do
-  resources :photos
-  resources :articles
-end
-
-
+  get "/blog/:slug", to: "blog#show"
 
 
 end
